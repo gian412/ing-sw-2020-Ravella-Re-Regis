826fc2c2ec9111715c885e37bfb8682c023d507e
rave98 <eliaravella@gmail.com>
2020-03-21 17:53:45 +0100

    only added class Controller.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,23 +1,14 @@
 package controller;
 
-import it.polimi.ingsw.model.Game;
-import it.polimi.ingsw.model.Player;
-import it.polimi.ingsw.model.Board;
-import it.polimi.ingsw.model.Cell;
-
+import it.polimi.ingsw.model.*;
 import java.util.Scanner;
 
 public class Controller {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    private Game game;
 
-        Game myGame = new Game();
-        myGame.addPlayer(new Player("p1"));
-        myGame.addPlayer(new Player("p2"));
-
-        Board gen = myGame.getBoard();
-        System.out.println(gen.toString());
-
+    public Controller(Game g){
+        this.game = g;
     }
+
 }
