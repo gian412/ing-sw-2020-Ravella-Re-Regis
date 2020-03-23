ff779cafea41ad4f527cf912f70273e1ece2d641
Gianluca Regis <gian.regis@gmail.com>
2020-03-24 00:41:29 +0100

    Implemented the new method makeMove for the class Hephaestus

--- src/main/java/it/polimi/ingsw/model/god/Hephaestus.java
+++ src/main/java/it/polimi/ingsw/model/god/Hephaestus.java
@@ -9,9 +9,48 @@ import it.polimi.ingsw.model.Height;
 
 public class Hephaestus extends God {
 
+    private boolean hadMove;
+    private boolean[] hadBuild = {false, false};
+    private  Cell previousCell = null;
+
     // class constructor with the initialization of board using the super constructor
     public Hephaestus(Board board) {
         super(board, "HEPHAESTUS");
+    }
+
+    @Override
+    public void makeMove(Worker worker, Command command) throws IllegalMoveException {
+
+        Cell cell = board.getCell(command.cellX, command.cellY);
+
+        switch (command.commandType){
+            case MOVE:
+                if (!hadMove && !hadBuild[0] && !hadBuild[1] && !hadWin){
+                    super.move(worker, cell);
+                    hadMove = true;
+                    hadWin = board.checkWin(worker);
+                    break;
+                } else{
+                    throw new IllegalMoveException();
+                }
+
+            case BUILD:
+                if (hadMove && !hadBuild[0] && !hadBuild[1] && !hadWin){
+                    super.build(worker, cell, false);
+                    previousCell = cell;
+                    hadBuild[0] = true;
+                    break;
+                } else if (hadMove && hadBuild[0] && !hadBuild[1] && !hadWin && previousCell!=null && (previousCell.equals(cell)) && cell.getHeight()!=Height.THIRD_FLOOR && cell.getHeight()!=Height.DOME){
+                    super.build(worker, cell, false);
+                    hadBuild[1] = true;
+                    break;
+                } else{
+                    throw new IllegalMoveException();
+                }
+
+            case BUILD_DOME:
+                throw new IllegalMoveException();
+        }
     }
 
     /*// array cell composed by 3 cells, 1 for the moves and 2 for the build
@@ -44,9 +83,4 @@ public class Hephaestus extends God {
         }
 
     }*/
-
-    @Override
-    public void makeMove(Worker worker, Command command) throws IllegalMoveException {
-
-    }
 }
