aa26d1ced79f9938619d08bb7b987f595e6e81eb
rave98 <eliaravella@gmail.com>
2020-03-25 17:20:45 +0100

    added BoardProxy class

--- src/main/java/it/polimi/ingsw/model/BoardProxy.java
+++ src/main/java/it/polimi/ingsw/model/BoardProxy.java
@@ -2,8 +2,25 @@ package it.polimi.ingsw.model;
 
 import it.polimi.ingsw.model.Board;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BoardProxy {
 
-    private Board boardCopy;
+    public final Height[][] boardScheme;
+    public final Map<String, Pair> workers;
+
+    public BoardProxy(){
+        boardScheme = new Height[5][5];
+        workers = new HashMap<String, Pair>();
+    }
+
+    public void addHeight(int x, int y, Height h){
+        boardScheme[x][y] = h;
+    }
+
+    public void addWorker(String worker, Pair coordinates){
+        workers.put(worker, coordinates);
+    }
 
 }
