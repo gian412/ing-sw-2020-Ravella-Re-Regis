08310a1fcd2bc7126953a6ed9877438344eb3e35
GianlucaRegis <10539584@polimi.it>
2020-03-27 02:17:34 +0100

    Created Javadoc for class Prometheus

--- src/main/java/it/polimi/ingsw/model/god/Prometheus.java
+++ src/main/java/it/polimi/ingsw/model/god/Prometheus.java
@@ -10,11 +10,29 @@ public class Prometheus extends God {
     private boolean hadMove = false;
     private boolean[] hadBuild = {false, false};
 
+    /**
+     * Class' constructor that use the super class' constructor
+     *
+     * @param board indicates the board of the game
+     */
     // class constructor with the initialization of board using the super constructor
     public Prometheus(Board board) {
         super(board, "PROMETHEUS");
     }
 
+    /**
+     * Actions made every turn
+     *
+     * Action made by the worker received by parameter. the possible moves are:
+     *      1- Move using super.move(Worker worker, Cell cell)
+     *      2- Build using super.build(Cell cell, boolean false)
+     * In this method, the worker can build before and after the move if it
+     * didn't move upward
+     *
+     * @param worker is the worker who is doing the actions
+     * @param command is the command which need to be interpreted
+     * @throws IllegalMoveException in case the action isn't legal
+     */
     @Override
     public void makeMove(Worker worker, Command command) throws IllegalMoveException {
         if (command!=null){
@@ -50,40 +68,5 @@ public class Prometheus extends God {
             }
         }
     }
-
-    /*// array cell composed by 2 cells, 1 for the moves and 2 for the build
-    @Override
-    public void makeMove(Worker worker, Cell[] cells, boolean isDome) throws IllegalMoveException, NullPointerException {
-
-
-
-        // first build
-        if( worker != null ){
-            if( cells[0] != null ){
-                if( worker.getCurrentCell().getHeight() == cells[1].getHeight() ){
-                    super.build(worker, cells[0], false );
-                } else{
-                    throw new IllegalMoveException();
-                }
-            }
-        } else{
-            throw new NullPointerException();
-        }
-
-        // move
-        if( cells[1] != null ){
-            super.move(worker, cells[1]);
-        } else{
-            throw new NullPointerException();
-        }
-
-        if( !hadWin ){
-            // second build
-            if( cells[2] != null ){
-                super.build(worker, cells[2], false );
-            } else{
-                throw new NullPointerException();
-            }
-        }
-    }*/
+    
 }
