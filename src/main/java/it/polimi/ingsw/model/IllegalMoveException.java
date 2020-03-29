15faf01ee754a68911087e7f3e7135064c075d6d
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-03-29 15:43:19 +0200

    Added Javadoc to class IllegalMoveException

--- src/main/java/it/polimi/ingsw/model/IllegalMoveException.java
+++ src/main/java/it/polimi/ingsw/model/IllegalMoveException.java
@@ -2,6 +2,9 @@ package it.polimi.ingsw.model;
 
 public class IllegalMoveException extends Exception{
 
+    /**
+     * Class constructor with the initialization of the error message
+     */
     public IllegalMoveException(){
         super("Illegal move");
     }
