e61598244560a4716d880b8f49993244470d7938
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-03-31 03:33:03 +0200

    Fixed some bugs in test classes

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -2,7 +2,9 @@ package it.polimi.ingsw;
 
 import static org.junit.Assert.assertTrue;
 
+import it.polimi.ingsw.model.god.Apollo;
 import it.polimi.ingsw.model.god.ApolloTest;
+import it.polimi.ingsw.model.god.Artemis;
 import it.polimi.ingsw.model.god.ArtemisTest;
 
 import org.junit.Test;
@@ -21,10 +23,25 @@ public class AppTest
         assertTrue( true );
     }
 
-    /*public static void main(String[] args){
-        GodTest godTest = new GodTest();
-        godTest.checkWinYesTest();
-        godTest.checkWinNoTest();
+    public static void main(String[] args){
+
+        // class Apollo
+        ApolloTest apolloTest = new ApolloTest();
+        apolloTest.hadMoveTest();
+        apolloTest.hadMoveAndForcedTest();
+        apolloTest.hadBuildNotDomeTest();
+        apolloTest.hadBuildDomeTest();
+        apolloTest.hadWinYesTest();
+        apolloTest.hadWinNoTest();
+
+        // class Artemis
+        ArtemisTest artemisTest = new ArtemisTest();
+        artemisTest.hadMoveTest();
+        artemisTest.hadMoveSecondTest();
+        artemisTest.hadBuildNotDomeTest();
+        artemisTest.hadBuildDomeTest();
+        artemisTest.hadWinYesTest();
+        artemisTest.hadWinNoTest();
     }
-    */
+
 }
