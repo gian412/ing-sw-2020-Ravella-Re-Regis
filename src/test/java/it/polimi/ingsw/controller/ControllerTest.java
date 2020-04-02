7bb6e4515e1d8b5d319b422476cd45316493ab84
rave98 <eliaravella@gmail.com>
2020-04-02 12:30:01 +0200

    added tests for Controller

--- src/test/java/it/polimi/ingsw/controller/ControllerTest.java
+++ src/test/java/it/polimi/ingsw/controller/ControllerTest.java
@@ -64,6 +64,32 @@ public class ControllerTest {
     }
 
     @Test
+    @DisplayName("Check the addWorker function")
+    public void checkAddWorker(){
+        Game g = new Game();
+        Controller controller = new Controller(g);
+
+        controller.addPlayer("Marco", 30);
+        controller.addPlayer("Gianluca", 35);
+
+        try {
+            g.setPlayerDivinity("Marco", new Apollo(g.getBoard()));
+            g.setPlayerDivinity("Gianluca", new Athena(g.getBoard()));
+        }catch(NoSuchPlayerException x){
+            System.err.println(x.getMessage());
+        }
+
+        controller.startGame();
+        controller.addWorker(1, 0);
+
+        assertNotNull("a worker must be in (1, 0)", g.getBoard().getCell(1, 0).getWorker());
+        assertEquals("controller.addWorker adds a playerTurn's worker",
+                g.getBoard().getCell(1, 0).getWorker().getOwner(),
+                g.getTurnPlayer()
+        );
+    }
+
+    @Test
     @DisplayName("test the build function")
     public void checkBuild(){
         Game g = new Game();
@@ -80,9 +106,9 @@ public class ControllerTest {
         }catch(NoSuchPlayerException x){
             System.err.println(x.getMessage());
         }
-        /**
-         * TODO add the workers to the board
-         */
+
+        controller.startGame();
+        controller.addWorker(1, 0);
         controller.commitMove("Marco", new Command(0, 0, CommandType.BUILD), 0);
 
         assertEquals("Built a floor in a cell", g.getBoard().getCell(0, 0).getHeight(), Height.FIRST_FLOOR);
@@ -105,15 +131,11 @@ public class ControllerTest {
             System.err.println(x.getMessage());
         }
 
-        /**
-         * TODO add the workers to the board (one in (0, 0))
-         */
-
+        controller.startGame();
+        controller.addWorker(0, 0);
         controller.commitMove("Marco", new Command(1, 1, CommandType.MOVE), 0);
 
         assertNotNull("moved a worker in (1, 1)", g.getBoard().getCell(1, 1).getWorker());
-
-
     }
 
 
