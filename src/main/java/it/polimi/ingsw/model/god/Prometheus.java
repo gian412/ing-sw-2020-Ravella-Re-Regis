66983b4b2a046776fe2a69da9fc73d698aa0be5a
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-04-21 13:19:24 +0200

    Refactored Pan and Prometheus classes

--- src/main/java/it/polimi/ingsw/model/god/Prometheus.java
+++ src/main/java/it/polimi/ingsw/model/god/Prometheus.java
@@ -36,14 +36,13 @@ public class Prometheus extends God {
     @Override
     public void executeCommand(Worker worker, Command command) throws IllegalMoveException, NullPointerException {
         if (command!=null){
-            Cell cell = board.getCell(command.cellX, command.cellY);
 
             switch (command.commandType){
                 case MOVE:
                     if (!hasMoved && !hasBuild && !hasWon && (!hasBuildBefore || worker.getPreviousCell().getHeight().getDifference(worker.getCurrentCell().getHeight())<1)){
 
                         try {
-                            super.move(worker, cell);
+                            super.move(worker, new Pair(command.cellX, command.cellY));
                             hasMoved = true;
                             hasWon = board.checkWin(worker);
                             break;
@@ -57,11 +56,11 @@ public class Prometheus extends God {
 
                 case BUILD:
                     if ( !hasBuildBefore && !hasMoved && !hasBuild && !hasWon ){
-                        super.build(worker.getCurrentCell(), cell, false);
+                        super.build(worker.getCurrentCell(), new Pair(command.cellX, command.cellY), false);
                         hasBuildBefore = true;
                         break;
                     } else if ( hasMoved && !hasBuild && !hasWon ){
-                        super.build(worker.getCurrentCell(), cell, false);
+                        super.build(worker.getCurrentCell(), new Pair(command.cellX, command.cellY), false);
                         hasBuild = true;
                         break;
                     } else {
@@ -69,12 +68,13 @@ public class Prometheus extends God {
                     }
 
                 case BUILD_DOME:
+                    Cell cell = board.getCell(command.cellX, command.cellY); // Get the reference to the cell
                     if ( !hasBuildBefore && !hasMoved && !hasBuild && !hasWon && cell.getHeight() == Height.THIRD_FLOOR){
-                        super.build(worker.getCurrentCell(), cell, false);
+                        super.build(worker.getCurrentCell(), new Pair(command.cellX, command.cellY), false);
                         hasBuildBefore = true;
                         break;
                     } else if ( hasMoved && !hasBuild && !hasWon && cell.getHeight() == Height.THIRD_FLOOR ){
-                        super.build(worker.getCurrentCell(), cell, false);
+                        super.build(worker.getCurrentCell(), new Pair(command.cellX, command.cellY), false);
                         hasBuild = true;
                         break;
                     } else {
@@ -92,83 +92,6 @@ public class Prometheus extends God {
             throw new NullPointerException();
         }
     }
-
-    /*public void makeMove(Worker worker, Command command) throws IllegalMoveException, NullPointerException {
-        if (command!=null){
-            Cell cell = board.getCell(command.cellX, command.cellY);
-
-            switch (command.commandType){
-                case MOVE:
-                    if (!hasMoved && !hasBuildSecond && !hasWon){
-                        if ( hasBuild && worker.getCurrentCell().getHeight().getDifference(cell.getHeight())<1 ){
-                            try {
-                                super.move(worker, cell);
-                                hasMoved = true;
-                                hasWon = board.checkWin(worker);
-                                break;
-                            } catch (IllegalMoveException e) {
-                                throw new IllegalMoveException();
-                            }
-                        } else{
-                            throw new IllegalMoveException();
-                        }
-                    } else{
-                        throw new IllegalMoveException();
-                    }
-
-                case BUILD:
-                    if (!hasBuild && !hasMoved && !hasBuildSecond && !hasWon){
-                        try {
-                            super.build(worker.getCurrentCell(), cell, false);
-                            hasBuild = true;
-                            break;
-                        } catch (IllegalMoveException e) {
-                            throw new IllegalMoveException();
-                        }
-                    } else if(hasMoved && !hasBuildSecond && !hasWon){
-                        try {
-                            super.build(worker.getCurrentCell(), cell, false);
-                            hasBuildSecond = true;
-                            break;
-                        } catch (IllegalMoveException e) {
-                            throw new IllegalMoveException();
-                        }
-                    } else{
-                        throw new IllegalMoveException();
-                    }
-
-                case BUILD_DOME:
-                    if (!hasBuild && !hasMoved && !hasBuildSecond && !hasWon && cell.getHeight() == Height.THIRD_FLOOR){
-                        try {
-                            super.build(worker.getCurrentCell(), cell, false);
-                            hasBuild = true;
-                            break;
-                        } catch (IllegalMoveException e) {
-                            throw new IllegalMoveException();
-                        }
-                    } else if(hasMoved && !hasBuildSecond && !hasWon && cell.getHeight() == Height.THIRD_FLOOR){
-                        try {
-                            super.build(worker.getCurrentCell(), cell, false);
-                            hasBuildSecond = true;
-                            break;
-                        } catch (IllegalMoveException e) {
-                            throw new IllegalMoveException();
-                        }
-                    } else{
-                        throw new IllegalMoveException();
-                    }
-
-                case RESET:
-                    this.resetLocalVariables();
-                    break;
-
-                default:
-                    throw new IllegalMoveException();
-            }
-        } else{
-            throw new NullPointerException();
-        }
-    }*/
 
     /**
      * Reset local variable for class Prometheus using the super method and adding local variables
