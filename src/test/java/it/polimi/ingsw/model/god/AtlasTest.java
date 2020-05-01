19ac2706ccd92f360388e66ca1906c9575b74f05
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-05-01 18:55:49 +0200

    Enhanced test quality for AtlasTest class

--- src/test/java/it/polimi/ingsw/model/god/AtlasTest.java
+++ src/test/java/it/polimi/ingsw/model/god/AtlasTest.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.*;
 
 public class AtlasTest {
 
+    // Common tests
     @Test
     @DisplayName("hasMoved")
     public void hasMovedTest(){
@@ -24,7 +25,7 @@ public class AtlasTest {
         Worker worker = new Worker("Id", player);
 
         // Initialization of the first cell
-        Cell firstCell = board.getCell(new Pair(0,1));
+        Cell firstCell = board.getCell(new Pair(0, 1));
         firstCell.setHeight(Height.FIRST_FLOOR);
         firstCell.setWorker(worker);
 
@@ -46,6 +47,106 @@ public class AtlasTest {
             System.err.println("Error e in method hasMovedTest in class AtlasTest: " + e.toString());
             fail("Exception in hasMovedTest in class AtlasTest");
         }
+
+    }
+
+    @Test
+    @DisplayName("canMoveUp=false")
+    public void moveWithNotCanMoveUp(){
+
+        // Initialization of the parameters
+        Board board = new Board();
+        Command command = new Command(new Pair(1, 1), CommandType.MOVE);
+        God god = new Atlas(board);
+        Player player = new Player("Name", 18);
+        player.setDivinity(god);
+        Worker worker = new Worker("Id", player);
+
+        // Initialization of the first cell
+        Cell firstCell = board.getCell(new Pair(0, 1));
+        firstCell.setHeight(Height.FIRST_FLOOR);
+        firstCell.setWorker(worker);
+
+        // Initialization of the second cell
+        Cell secondCell = board.getCell(new Pair(1, 1));
+        secondCell.setHeight(Height.SECOND_FLOOR);
+        secondCell.setWorker(null);
+
+        worker.setCurrentCell(firstCell);
+        worker.setCanMoveUp(false);
+
+        try {
+            god.executeCommand(worker, command);
+            fail("moveWithNotCanMoveUp in class AtlasTest didn't throw an exception");
+        } catch (IllegalMoveException e) {
+            assertEquals("worker's position must be firstCell", worker.getCurrentCell(), firstCell);
+        }
+    }
+
+    @Test
+    @DisplayName("moveUpMoreThan1Floor")
+    public void moveUpMoreThan1Floor() {
+
+        // Initialization of the parameters
+        Board board = new Board();
+        Command command = new Command(new Pair(1, 1), CommandType.MOVE);
+        God god = new Atlas(board);
+        Player player = new Player("Name", 18);
+        player.setDivinity(god);
+        Worker worker = new Worker("Id", player);
+
+        // Initialization of the first cell
+        Cell firstCell = board.getCell(new Pair(0, 1));
+        firstCell.setHeight(Height.GROUND);
+        firstCell.setWorker(worker);
+
+        // Initialization of the second cell
+        Cell secondCell = board.getCell(new Pair(1, 1));
+        secondCell.setHeight(Height.SECOND_FLOOR);
+        secondCell.setWorker(null);
+
+        worker.setCurrentCell(firstCell);
+
+        try {
+            god.executeCommand(worker, command);
+            fail("moveUpMoreThan1Floor in class AtlasTest didn't throw an exception");
+        } catch (IllegalMoveException e) {
+            assertEquals("worker's position must be firstCell", worker.getCurrentCell(), firstCell);
+        }
+
+    }
+
+    @Test
+    @DisplayName("moveOnADome")
+    public void moveOnADome() {
+
+        // Initialization of the parameters
+        Board board = new Board();
+        Command command = new Command(new Pair(1, 1), CommandType.MOVE);
+        God god = new Atlas(board);
+        Player player = new Player("Name", 18);
+        player.setDivinity(god);
+        Worker worker = new Worker("Id", player);
+
+        // Initialization of the first cell
+        Cell firstCell = board.getCell(new Pair(0, 1));
+        firstCell.setHeight(Height.THIRD_FLOOR);
+        firstCell.setWorker(worker);
+
+        // Initialization of the second cell
+        Cell secondCell = board.getCell(new Pair(1, 1));
+        secondCell.setHeight(Height.DOME);
+        secondCell.setWorker(null);
+
+        worker.setCurrentCell(firstCell);
+
+        try {
+            god.executeCommand(worker, command);
+            fail("moveOnADome in class AtlasTest didn't throw an exception");
+        } catch (IllegalMoveException e) {
+            assertEquals("worker's position must be firstCell", worker.getCurrentCell(), firstCell);
+        }
+
     }
 
     @Test
@@ -85,6 +186,151 @@ public class AtlasTest {
         }
     }
 
+    @Test
+    @DisplayName("hasBuild a dome")
+    public void hasBuildDomeTest(){
+
+        // Initialization of the parameters
+        Board board = new Board();
+        Command command = new Command(new Pair(1, 1), CommandType.BUILD_DOME);
+        God god = new Atlas(board);
+        Player player = new Player("Name", 18);
+        player.setDivinity(god);
+        Worker worker = new Worker("Id", player);
+        god.hasMoved = true;
+
+        // Initialization of the first cell
+        Cell firstCell = board.getCell(new Pair(0, 1));
+        firstCell.setHeight(Height.SECOND_FLOOR);
+        firstCell.setWorker(worker);
+
+        // Initialization of the second cell
+        Cell secondCell = board.getCell(new Pair(1, 1));
+        secondCell.setHeight(Height.THIRD_FLOOR);
+        secondCell.setWorker(null);
+
+        worker.setCurrentCell(firstCell);
+
+        try {
+            god.executeCommand(worker, command);
+
+            assertTrue("hasBuild must be true", god.hasBuild);
+            assertEquals("secondCell's Height must be equals to DOME", secondCell.getHeight(), Height.DOME);
+
+        } catch (IllegalMoveException e) {
+            System.err.println("Error e in method hasBuildDomeTest in class AtlasTest: " + e.toString());
+            fail("Exception in hasBuildDomeTest in class AtlasTest");
+        }
+
+    }
+
+    @Test
+    @DisplayName("has build on a dome")
+    public void hasBuildOnADome() {
+
+        // Initialization of the parameters
+        Board board = new Board();
+        Command command = new Command(new Pair(1, 1), CommandType.BUILD_DOME);
+        God god = new Atlas(board);
+        Player player = new Player("Name", 18);
+        player.setDivinity(god);
+        Worker worker = new Worker("Id", player);
+        god.hasMoved = true;
+
+        // Initialization of the first cell
+        Cell firstCell = board.getCell(new Pair(0, 1));
+        firstCell.setHeight(Height.SECOND_FLOOR);
+        firstCell.setWorker(worker);
+
+        // Initialization of the second cell
+        Cell secondCell = board.getCell(new Pair(1, 1));
+        secondCell.setHeight(Height.DOME);
+        secondCell.setWorker(null);
+
+        worker.setCurrentCell(firstCell);
+
+        try {
+            god.executeCommand(worker, command);
+            fail("moveOnADome in class AtlasTest didn't throw an exception");
+        } catch (IllegalMoveException e) {
+            assertEquals("secondCell's Height must be equals to DOME", secondCell.getHeight(), Height.DOME);
+        }
+
+    }
+
+    @Test
+    @DisplayName("hasWon = true")
+    public void hasWonTrueTest(){
+
+        // Initialization of the parameters
+        Board board = new Board();
+        Command command = new Command(new Pair(1, 1), CommandType.MOVE);
+        God god = new Atlas(board);
+        Player player = new Player("Name", 18);
+        player.setDivinity(god);
+        Worker worker = new Worker("Id", player);
+
+        // Initialization of the first cell
+        Cell firstCell = board.getCell(new Pair(0, 1));
+        firstCell.setHeight(Height.SECOND_FLOOR);
+        firstCell.setWorker(worker);
+
+        // Initialization of the second cell
+        Cell secondCell = board.getCell(new Pair(1, 1));
+        secondCell.setHeight(Height.THIRD_FLOOR);
+        secondCell.setWorker(null);
+
+        worker.setCurrentCell(firstCell);
+
+        try{
+            god.executeCommand(worker, command);
+
+            assertTrue( "hasWon must be true", god.hasWon );
+            assertEquals("worker's position's Height must be THIRD_FLOOR", worker.getCurrentCell().getHeight(), Height.THIRD_FLOOR);
+
+        } catch (IllegalMoveException e){
+            System.err.println("Error e in method hasWonTrueTest in class AtlasTest: " + e.toString());
+            fail("Exception in hasWonTrueTest in class AtlasTest");
+        }
+    }
+
+    @Test
+    @DisplayName("hasWon = false")
+    public void hasWonFalseTest(){
+
+        // Initialization of the parameters
+        Board board = new Board();
+        Command command = new Command(new Pair(1, 1), CommandType.MOVE);
+        God god = new Atlas(board);
+        Player player = new Player("Name", 18);
+        player.setDivinity(god);
+        Worker worker = new Worker("Id", player);
+
+        // Initialization of the first cell
+        Cell firstCell = board.getCell(new Pair(0, 1));
+        firstCell.setHeight(Height.THIRD_FLOOR);
+        firstCell.setWorker(worker);
+
+        // Initialization of the second cell
+        Cell secondCell = board.getCell(new Pair(1, 1));
+        secondCell.setHeight(Height.THIRD_FLOOR);
+        secondCell.setWorker(null);
+
+        worker.setCurrentCell(firstCell);
+
+        try{
+            god.executeCommand(worker, command);
+
+            assertFalse( "hasWon must be false", god.hasWon );
+            assertEquals("Worker must be on the third floor", worker.getCurrentCell().getHeight(), Height.THIRD_FLOOR);
+
+        } catch (IllegalMoveException e){
+            System.err.println("Error e in method hasWonFalseTest in class AtlasTest: " + e.toString());
+            fail("Exception in hasWonFalseTest in class AtlasTest");
+        }
+    }
+    
+    // Exclusive tests
     @Test
     @DisplayName("hasBuild a dome in right position")
     public void hasBuildDomeInRightPositionTest(){
@@ -156,78 +402,6 @@ public class AtlasTest {
         } catch (IllegalMoveException e) {
             System.err.println("Error e in method hasBuildDomeTest in class AtlasTest: " + e.toString());
             fail("Exception in hasBuildDomeTest in class AtlasTest");
-        }
-    }
-
-    @Test
-    @DisplayName("hasWon = true")
-    public void hasWonTrueTest(){
-
-        // Initialization of the parameters
-        Board board = new Board();
-        Command command = new Command(new Pair(1, 1), CommandType.MOVE);
-        God god = new Atlas(board);
-        Player player = new Player("Name", 18);
-        player.setDivinity(god);
-        Worker worker = new Worker("Id", player);
-
-        // Initialization of the first cell
-        Cell firstCell = board.getCell(new Pair(0, 1));
-        firstCell.setHeight(Height.SECOND_FLOOR);
-        firstCell.setWorker(worker);
-
-        // Initialization of the second cell
-        Cell secondCell = board.getCell(new Pair(1, 1));
-        secondCell.setHeight(Height.THIRD_FLOOR);
-        secondCell.setWorker(null);
-
-        worker.setCurrentCell(firstCell);
-
-        try{
-            god.executeCommand(worker, command);
-
-            assertTrue( "hasWon must be true", god.hasWon );
-            assertEquals("worker's position's Height must be THIRD_FLOOR", worker.getCurrentCell().getHeight(), Height.THIRD_FLOOR);
-
-        } catch (IllegalMoveException e){
-            System.err.println("Error e in method hasWonTrueTest in class AtlasTest: " + e.toString());
-            fail("Exception in hasWonTrueTest in class AtlasTest");
-        }
-    }
-
-    @Test
-    @DisplayName("hasWon = false")
-    public void hasWonFalseTest(){
-
-        // Initialization of the parameters
-        Board board = new Board();
-        Command command = new Command(new Pair(1, 1), CommandType.MOVE);
-        God god = new Atlas(board);
-        Player player = new Player("Name", 18);
-        player.setDivinity(god);
-        Worker worker = new Worker("Id", player);
-
-        // Initialization of the first cell
-        Cell firstCell = board.getCell(new Pair(0, 1));
-        firstCell.setHeight(Height.SECOND_FLOOR);
-        firstCell.setWorker(worker);
-
-        // Initialization of the second cell
-        Cell secondCell = board.getCell(new Pair(1, 1));
-        secondCell.setHeight(Height.SECOND_FLOOR);
-        secondCell.setWorker(null);
-
-        worker.setCurrentCell(firstCell);
-
-        try{
-            god.executeCommand(worker, command);
-
-            assertFalse( "hasWon must be false", god.hasWon );
-            assertNotEquals("Worker can't be on the third floor", worker.getCurrentCell().getHeight(), Height.THIRD_FLOOR);
-
-        } catch (IllegalMoveException e){
-            System.err.println("Error e in method hasWonFalseTest in class AtlasTest: " + e.toString());
-            fail("Exception in hasWonFalseTest in class AtlasTest");
         }
     }
 
