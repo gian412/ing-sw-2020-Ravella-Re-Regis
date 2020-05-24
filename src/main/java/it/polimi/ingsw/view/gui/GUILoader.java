e7aea9f0c0100a891778c326dfbe678db3ac4c55
rave98 <eliaravella@gmail.com>
2020-05-24 10:07:52 +0200

    drafted a socket listener for the GUI

--- src/main/java/it/polimi/ingsw/view/gui/GUILoader.java
+++ src/main/java/it/polimi/ingsw/view/gui/GUILoader.java
@@ -1,11 +1,38 @@
 package it.polimi.ingsw.view.gui;
 
 import javax.swing.*;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class GUILoader extends JFrame{
 
-    public GUILoader(){
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame("Santorini");
+        Socket connSocket = null;
+        try {
+            connSocket = new Socket("127.0.0.1", 1337);
+            Scanner x = new Scanner(connSocket.getInputStream());
+            PrintStream y = new PrintStream(connSocket.getOutputStream());
+            y.println("GUIclient");
+            y.flush();
+            y.println(1);
+            y.flush();
+            x.nextLine();
+            x.nextLine();
 
+            GamePanel board = new GamePanel(connSocket);
+            mainFrame.add(board);
+            board.activeGamePanel();
+
+            mainFrame.setSize(500, 500);
+            mainFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            mainFrame.setVisible(true);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/it/polimi/ingsw/view/gui/GamePanel.java
+++ src/main/java/it/polimi/ingsw/view/gui/GamePanel.java
@@ -1,19 +1,42 @@
 package it.polimi.ingsw.view.gui;
 
+import it.polimi.ingsw.model.BoardProxy;
+import it.polimi.ingsw.view.BoardListener;
+import it.polimi.ingsw.view.Observer;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
 public class GamePanel extends JPanel {
-    public static void main(String[] args) throws IOException {
-        JFrame frame = new JFrame();
-        File prove = new File("..\\..\\..\\..\\..\\..\\graphics\\graphics_0000s_0001_god_and_hero_cards_0055_Tyche.png");
+    Socket socket;
 
-        BufferedImage myPicture = ImageIO.read(prove);
-        JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-        frame.add(picLabel);
+    class readProxyBoard implements Observer<BoardProxy>{
+        JPanel displayPanel;
 
+        public readProxyBoard(JPanel displayPanel){
+            this.displayPanel = displayPanel;
+        }
+
+        @Override
+        public void update(BoardProxy message) {
+            displayPanel.add(new JLabel(message.toString()));
+        }
+    }
+
+    public void activeGamePanel() throws IOException {
+        BoardListener bl = new BoardListener(new ObjectInputStream(socket.getInputStream()));
+        new ObjectOutputStream(socket.getOutputStream());
+        bl.addObserver(new readProxyBoard(this));
+        new Thread(bl).start();
+    }
+
+    public GamePanel(Socket connSocket){
+        this.socket = connSocket;
     }
 }
