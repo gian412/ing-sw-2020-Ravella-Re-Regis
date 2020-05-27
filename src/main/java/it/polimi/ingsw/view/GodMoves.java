ab4b5db1d60efab6e2064e1735b77a4effcae294
GianlucaRegis <gianluca.regis@mail.polimi.it>
2020-05-27 11:42:53 +0200

    Created godMoves for handling possible moves for every god

---
+++ src/main/java/it/polimi/ingsw/view/GodMoves.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.view;
+
+import java.util.HashMap;
+
+public final class GodMoves {
+
+    //private static HashMap<String, Enum CommandType>
+
+    //private (Enum possibleMove) setPlayersPossibleMove(String God) throws IllegalArgumentException {}
+
+}
