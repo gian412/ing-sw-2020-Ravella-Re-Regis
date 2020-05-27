c13a1af883cc0c0698e051df2cf0bfdb38834dfb
GianlucaRegis <gianluca.regis@mail.polimi.it>
2020-05-28 01:00:13 +0200

    Added all gods to GodMoves class and created a return method

--- src/main/java/it/polimi/ingsw/utils/GodMoves.java
+++ src/main/java/it/polimi/ingsw/utils/GodMoves.java
@@ -12,27 +12,68 @@ public final class GodMoves {
 
     }
 
-    private static void init() {
+    private void PossibleMove() {
 
         // Apollo
-        allPossibleMoves.put(GodType.APOLLO, new CommandType[]{CommandType.MOVE, CommandType.BUILD});
+        allPossibleMoves.put(GodType.APOLLO,
+                new CommandType[]{CommandType.MOVE, CommandType.BUILD});
 
         // Artemis
-        allPossibleMoves.put(GodType.ARTEMIS, new CommandType[]{CommandType.MOVE, CommandType.MOVE, CommandType.BUILD});
+        allPossibleMoves.put(GodType.ARTEMIS,
+                new CommandType[]{CommandType.MOVE, CommandType.MOVE, CommandType.BUILD});
 
         // Athena
-        allPossibleMoves.put(GodType.ATHENA, new CommandType[]{CommandType.MOVE, CommandType.BUILD});
+        allPossibleMoves.put(GodType.ATHENA,
+                new CommandType[]{CommandType.MOVE, CommandType.BUILD});
 
         // Atlas
-        allPossibleMoves.put(GodType.ATLAS, new CommandType[]{CommandType.MOVE, CommandType.BUILD});
+        allPossibleMoves.put(GodType.ATLAS,
+                new CommandType[]{CommandType.MOVE, CommandType.BUILD});
 
         // Charon
-        allPossibleMoves.put(GodType.CHARON, new CommandType[]{CommandType.MOVE, CommandType.BUILD, CommandType.FORCE});
+        allPossibleMoves.put(GodType.CHARON,
+                new CommandType[]{CommandType.MOVE, CommandType.BUILD, CommandType.FORCE});
 
         // Chronus
+        allPossibleMoves.put(GodType.CHRONUS,
+                new CommandType[]{CommandType.MOVE, CommandType.BUILD});
 
+        // Demeter
+        allPossibleMoves.put(GodType.DEMETER,
+                new CommandType[]{CommandType.MOVE, CommandType.BUILD, CommandType.BUILD});
 
+        // Hephaestus
+        allPossibleMoves.put(GodType.HEPHAESTUS,
+                new CommandType[]{CommandType.MOVE, CommandType.BUILD, CommandType.BUILD});
 
+        // Hestia
+        allPossibleMoves.put(GodType.HESTIA,
+                new CommandType[]{CommandType.MOVE, CommandType.BUILD, CommandType.BUILD});
+
+        // Minotaur
+        allPossibleMoves.put(GodType.MINOTAUR,
+                new CommandType[]{CommandType.MOVE, CommandType.BUILD});
+
+        // Pan
+        allPossibleMoves.put(GodType.PAN,
+                new CommandType[]{CommandType.MOVE, CommandType.BUILD});
+
+        // Prometheus
+        allPossibleMoves.put(GodType.PROMETHEUS,
+                new CommandType[]{CommandType.BUILD, CommandType.MOVE, CommandType.BUILD});
+
+        // Triton
+        allPossibleMoves.put(GodType.TRITON,
+                new CommandType[]{CommandType.MOVE, CommandType.BUILD});
+
+        // Zeus
+        allPossibleMoves.put(GodType.ZEUS,
+                new CommandType[]{CommandType.MOVE, CommandType.BUILD});
+
+    }
+
+    public static CommandType[] getPossibleMove(GodType godType) {
+        return allPossibleMoves.get(godType);
     }
 
 }
