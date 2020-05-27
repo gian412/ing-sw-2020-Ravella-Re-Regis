f132d4567043409163c2ab2739d5fde5492fc158
GianlucaRegis <gianluca.regis@mail.polimi.it>
2020-05-27 16:14:10 +0200

    Added 4 Divinities to GodMoves class

--- src/main/java/it/polimi/ingsw/view/GodMoves.java
+++ src/main/java/it/polimi/ingsw/view/GodMoves.java
@@ -1,11 +1,40 @@
 package it.polimi.ingsw.view;
 
+import it.polimi.ingsw.controller.CommandType;
+
 import java.util.HashMap;
 
 public final class GodMoves {
 
-    //private static HashMap<String, Enum CommandType>
+    private static final HashMap<String, CommandType[]> allPossibleMoves = new HashMap<>();
 
-    //private (Enum possibleMove) setPlayersPossibleMove(String God) throws IllegalArgumentException {}
+    public PossibleMove setPlayersPossibleMove(String God) throws IllegalArgumentException {
+
+        return new PossibleMove();
+
+    }
+
+    private static void init() {
+
+        // Apollo
+        allPossibleMoves.put("APOLLO", new CommandType[]{CommandType.MOVE, CommandType.BUILD});
+
+        // Artemis
+        allPossibleMoves.put("ARTEMIS", new CommandType[]{CommandType.MOVE, CommandType.MOVE, CommandType.BUILD});
+
+        // Athena
+        allPossibleMoves.put("ATHENA", new CommandType[]{CommandType.MOVE, CommandType.BUILD});
+
+        // Atlas
+        allPossibleMoves.put("ATLAS", new CommandType[]{CommandType.MOVE, CommandType.BUILD});
+
+        // Charon
+        allPossibleMoves.put("CHARON", new CommandType[]{CommandType.MOVE, CommandType.BUILD, CommandType.FORCE});
+
+        // Chronus
+
+
+
+    }
 
 }
