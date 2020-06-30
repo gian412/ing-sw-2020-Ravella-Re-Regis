455e6c42dcf2afdaf61e8d3ed1a32e41232714b2
GianlucaRegis <gianluca.regis@mail.polimi.it>
2020-07-01 00:37:15 +0200

    Implemented canMove() and canBuild for Pan

--- src/main/java/it/polimi/ingsw/model/god/Pan.java
+++ src/main/java/it/polimi/ingsw/model/god/Pan.java
@@ -46,6 +46,11 @@ public class Pan extends  God {
                             super.move(worker, command.coordinates);
                             hasMoved = true;
                             hasWon = board.checkWin(worker);
+                            if (!hasWon && !canBuild(worker)) {
+                                board.removeWorker(worker);
+                                worker.setPreviousCell(null);
+                                worker.setCurrentCell(null);
+                            }
                             break;
                         } catch (IllegalMoveException e) {
                             throw new IllegalMoveException(e.getMessage());
@@ -85,7 +90,12 @@ public class Pan extends  God {
                     break;
 
                 case CHECK_WORKERS:
-                    return;
+                    if (!canMove(worker)) {
+                        board.removeWorker(worker);
+                        worker.setPreviousCell(null);
+                        worker.setCurrentCell(null);
+                    }
+                    break;
 
                 default:
                     throw new IllegalMoveException("Command type not valid for the current god");
