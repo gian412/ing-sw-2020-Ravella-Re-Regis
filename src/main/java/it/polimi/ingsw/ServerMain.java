001b7c4955ec2fac7c043064a116621ce4e66897
GianlucaRegis <gianluca.regis@mail.polimi.it>
2020-07-03 01:54:08 +0200

    README and images added

--- src/main/java/it/polimi/ingsw/ServerMain.java
+++ src/main/java/it/polimi/ingsw/ServerMain.java
@@ -6,9 +6,38 @@ import java.io.IOException;
 public class ServerMain {
 
     public static void main(String[] args) {
-        Server server; // Istanzio il server
-        server = new Server(); // Inizializzo il server
-        server.run(); // Faccio partire il server
+
+        Server server; // Instantiate Server
+
+        switch (args.length) {
+
+            case 0:
+                server = new Server(); // Initialize Server
+                server.run(); // Run Server
+                break;
+
+            case 1:
+                try {
+                    int port = Integer.parseInt(args[0]);
+                    if (port > 1024 && port < 65535) {
+                        server = new Server(port); // Initialize server
+                        server.run(); // Run server
+                    } else {
+                        System.err.println( "'" + args[0] + "' " + "is not a port.");
+                        System.out.println("Insert a valid port number between 1025 and 65534 to use a custom port or nothing to use the default port (13300)");
+                    }
+                } catch (NumberFormatException e) {
+                    System.err.println( "'" + args[0] + "' " + "is not a port.");
+                    System.out.println("Insert a valid port number between 1025 and 65534 to use a custom port or nothing to use the default port (13300)");
+                }
+                break;
+
+            default:
+                System.err.println( "Invalid parameters.");
+                System.out.println("Insert a valid port number between 1025 and 65534 to use a custom port or nothing to use the default port (13300)");
+                break;
+
+        }
 
     }
 }
